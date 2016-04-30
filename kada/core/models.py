f79189aa2c3c8b118aa5bae4422af21d91f6e9d0
Frank <knarfytrebil@gmail.com>
2016-05-01 06:14:25 +0800

    add models

--- kada/core/models.py
+++ kada/core/models.py
@@ -1,5 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# 关注关系
+class Friend(models.Model):
+    pass
+
+# 影集
+class Gallery(models.Model):
+    pass
+
+# 照片
+class Photo(models.Model):
+    pass
+
+# 立面
+class Scene(models.Model):
+    pass
+
+# 立面模板
+class SceneTemplate(models.Model):
+    pass
+
+# 服务
+class Service(models.Model):
+    pass
+
+# 收藏
+class Favourite(models.Model):
+    pass
+
+# 赞
+class Like(models.Model):
+    pass
+
+# 广告
+class Advertise(models.Model):
+    pass
+
+# 打赏
+class Tip(models.Model):
+    pass
+
+# 消息
+class Message(models.Model):
+    pass
+
+# 反馈
+class Feedback(models.Model):
+    pass
+
