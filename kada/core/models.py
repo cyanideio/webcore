d62795e81100cf74ccf65d2e6f97dac22d4bd530
Frank <knarfytrebil@gmail.com>
2016-05-02 11:52:00 +0800

    fill the models

--- kada/core/models.py
+++ kada/core/models.py
@@ -3,14 +3,48 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
-# 关注关系
+# 影集类型
+GALLERY_TYPE = (
+    (0, _("Oscar")),    #奥斯卡
+    (1, _("Ishiyaki")), #研烧
+)
+
+class UserProfile(object):
+    """用户资料"""
+    user = models.OneToOneField(User,blank=True, null=True)
+    avatar = models.ImageField(upload_to="avatar",blank=True,null=True)
+    gender = models.IntegerField(verbose_name=_("Gender"), choices=GENDER_CHOICES, default=2)
+    intro = models.TextField(blank=True, null=True)
+    mobile = models.CharField(max_length=11, unique=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    birthday = models.DateField(editable=True, blank=True, null=True)
+    money = models.FloatField(default=0)    
+    nickname = models.CharField(max_length=20,blank=True,null=True)
+    oauth_token = models.CharField(max_length=100, blank=True,null=True)   #  第三个方登录返回的用户凭证
+
 class Friend(models.Model):
-    pass
+    """关注关系"""
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    follower = models.ForeignKey(User, related_name="friend_follower")
+    followee = models.ForeignKey(User, related_name="friend_followee")
 
-# 影集
 class Gallery(models.Model):
-    pass
+    """影集"""
+    author = models.ForeignKey(User, related_name="gallery_author")
+    type_kbn = models.IntegerField(choices = GALLERY_TYPE) 
+    comment = models.ForeignKey(Comment, related_name="gallery_comment")
+    like = models.ForeignKey(Comment, related_name="gallery_comment")
+    favourite = models.ForeignKey(Comment, related_name="gallery_comment")
+    descriptio = models.TextField(blank=True, null=True)n
+    photo = models.ImageField(upload_to="avatar",blank=True,null=True)
+    scenes = models.ForeignKey(Scene, related_name="gallery_scene") 
+
+class Comment(models.Model):
+    """评论"""
+        
 
 # 照片
 class Photo(models.Model):
