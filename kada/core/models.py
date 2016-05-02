f8df8fc8c65abba34a65a3f8317574d7f8b72c59
Frank <knarfytrebil@gmail.com>
2016-05-02 13:37:52 +0800

    fix

--- kada/core/models.py
+++ kada/core/models.py
@@ -12,8 +12,17 @@ GALLERY_TYPE = (
     (1, _("Ishiyaki")), #研烧
 )
 
-class UserProfile(object):
-    """用户资料"""
+class UserProfile(models.Model):
+    """用户资料
+    Attributes:
+        user: 用户
+        avatar: 用户头像
+        intro: 介绍
+        location: 地址
+        money: 钱
+        nickname: 昵称
+        oauth_token: 第三个方登录返回的用户凭证
+    """
     user = models.OneToOneField(User,blank=True, null=True)
     avatar = models.ImageField(upload_to="avatar",blank=True,null=True)
     gender = models.IntegerField(verbose_name=_("Gender"), choices=GENDER_CHOICES, default=2)
@@ -23,40 +32,69 @@ class UserProfile(object):
     birthday = models.DateField(editable=True, blank=True, null=True)
     money = models.FloatField(default=0)    
     nickname = models.CharField(max_length=20,blank=True,null=True)
-    oauth_token = models.CharField(max_length=100, blank=True,null=True)   #  第三个方登录返回的用户凭证
+    oauth_token = models.CharField(max_length=100, blank=True,null=True)
 
 class Friend(models.Model):
-    """关注关系"""
+    """关注关系
+    Attributes:
+        eggs: An integer count of the eggs we have laid.
+    """
     created = models.DateTimeField(auto_now_add=True, editable=False)
     follower = models.ForeignKey(User, related_name="friend_follower")
     followee = models.ForeignKey(User, related_name="friend_followee")
 
 class Gallery(models.Model):
-    """影集"""
+    """影集
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
     author = models.ForeignKey(User, related_name="gallery_author")
     type_kbn = models.IntegerField(choices = GALLERY_TYPE) 
     comment = models.ForeignKey(Comment, related_name="gallery_comment")
     like = models.ForeignKey(Comment, related_name="gallery_comment")
     favourite = models.ForeignKey(Comment, related_name="gallery_comment")
-    descriptio = models.TextField(blank=True, null=True)n
+    description = models.TextField(blank=True, null=True)n
     photo = models.ImageField(upload_to="avatar",blank=True,null=True)
     scenes = models.ForeignKey(Scene, related_name="gallery_scene") 
 
 class Comment(models.Model):
-    """评论"""
+    """评论
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
         
 
-# 照片
 class Photo(models.Model):
-    pass
+    """照片
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
+    exif = models.TextField(blank=True, null=True)
+    url = models.ImageField(upload_to="photography",blank=True,null=True)
 
-# 立面
 class Scene(models.Model):
-    pass
+    """立面
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
+    gallery = models.ForeignKey(Gallery, related_name='scene_gallery')
+    scene_template = models.ForeignKey(SceneTemplate, related_name='scene_scene_template')
+    photos = models.CommaSeparatedIntegerField() #List of Photo Ids
 
-# 立面模板
 class SceneTemplate(models.Model):
-    pass
+    """立面模板
+
+    Attributes:
+        likes_spam: A boolean indicating if we like SPAM or not.
+        eggs: An integer count of the eggs we have laid.
+    """
+    cover = models.ImageField(upload_to="scene_cover",blank=True,null=True) #立面封面
+    background = models.ImageField(upload_to="scene_background",blank=True,null=True) #立面背景
+    capacity = models.IntegerField() #立面容量
 
 # 服务
 class Service(models.Model):
