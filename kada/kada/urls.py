f86ea99864d26d1eadfb58cac99bb8fe45c45eaf
Deploy Server <deployserver@deployserver.local>
2016-05-04 17:16:05 +0800

    fix

--- kada/kada/urls.py
+++ kada/kada/urls.py
@@ -1,21 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 """kada URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+# APIs
+所有的API URL都在这里定义
 """
+
+API_VERSION = 1
+API_V_STRING = "v%s" % API_VERSION
+
 from django.conf.urls import include, url
 from django.contrib import admin
 
+# APIs
+from core.api.friend import FriendResource
+from core.api.advertise import AdvertiseResource
+from core.api.comment import CommentResource
+from core.api.feedback import FeedbackResource
+from core.api.gallery import GalleryResource
+from core.api.message import MessageResource
+from core.api.scene import SceneResource
+from core.api.scene_template import SceneTemplateResource
+from core.api.service import ServiceResource
+
+v_api = Api(api_name=API_V_STRING)
+
+# 在当前的API重注册所有的 Resource
+v_api.register(FriendResource())
+v_api.register(AdvertiseResource())
+v_api.register(CommentResource())
+v_api.register(FeedbackResource())
+v_api.register(GalleryResource())
+v_api.register(MessageResource())
+v_api.register(SceneResource())
+v_api.register(SceneTemplateResource())
+v_api.register(ServiceResource())
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(v_api.urls)),
 ]
