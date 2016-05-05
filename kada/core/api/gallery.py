9a1c9faaeff1a2e1d7cfb9fb6af7779d3e1020b3
Frank <knarfytrebil@gmail.com>
2016-05-06 07:21:52 +0800

    fix gallery list

--- kada/core/api/gallery.py
+++ kada/core/api/gallery.py
@@ -3,8 +3,13 @@
 from tastypie import fields
 from kada.utils.resource import KadaResource
 from kada.utils.custom_fields import CommaSeparatedIntegerField
-from core.models import Gallery, Photo, Scene
+from core.models import Gallery, Photo, Scene, SceneTemplate
 from core.api.user import UserResource
+
+class SceneTemplateResource(KadaResource):
+    """docstring for PhotoResource"""
+    class Meta:
+        queryset = SceneTemplate.objects.all()
 
 class PhotoResource(KadaResource):
     """docstring for PhotoResource"""
@@ -13,6 +18,7 @@ class PhotoResource(KadaResource):
 
 class SceneResource(KadaResource):
     """docstring for SceneResource"""
+    scene_template = fields.ToOneField(SceneTemplateResource, 'scene_template', related_name='scene_scene_template', full=True)
     class Meta:
         queryset = Scene.objects.all()
 
@@ -20,7 +26,7 @@ class GalleryResource(KadaResource):
     """影集"""
     author = fields.ToOneField(UserResource, 'author', null=True, full=True)
     scene_seq = CommaSeparatedIntegerField(attribute='scene_seq')
-    photos = fields.ToManyField(PhotoResource, 'photos', null=True, full=True)
-    scenes = fields.ToManyField(SceneResource, 'scenes', null=True, full=True)
+    photos = fields.ToManyField(PhotoResource, 'photo_gallery', null=True, full=True)
+    scenes = fields.ToManyField(SceneResource, 'scene_gallery', null=True, full=True)
     class Meta:
         queryset = Gallery.objects.all()
