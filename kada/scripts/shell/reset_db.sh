ec7973b56023e101685700ca2dc601a88c5077d5
Deploy Server <deployserver@deployserver.local>
2016-05-09 16:32:50 +0800

    fix

--- kada/scripts/shell/reset_db.sh
+++ kada/scripts/shell/reset_db.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 echo "Cleaning Up..."
-if [$KADA_ENV == 'local']; then
+if [ "$KADA_ENV" = "local" ]; then
 	rm db.sqlite3
 	rm -rf core/migrations
 else
@@ -9,7 +10,7 @@ else
 		CREATE DATABASE kada_db;
 		ALTER DATABASE kada_db CHARACTER SET utf8 COLLATE utf8_general_ci;
 	"	
-fi
+fi;
 echo "Initializing..."
 python manage.py migrate
 python manage.py makemigrations core
@@ -17,4 +18,3 @@ python manage.py migrate core
 echo "Loading Data..."
 python scripts/py/load_test_data.py
 echo "Finished Loading Data..."
-
