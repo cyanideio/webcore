3c7e1e3b71c33a66b59e7afd677dd5d326e8808f
root <frank.yao@cyanide.io>
2016-05-23 14:05:00 +0800

    fix

--- kada/kada/settings.py
+++ kada/kada/settings.py
@@ -22,7 +22,7 @@ if KADA_ENV != 'local':
     OSS_ACCESS_URL = 'oss-cn-beijing.aliyuncs.com'
     OSS_ACCESS_KEY_ID = 'Mzlk1jnj5jTrrMT0'
     OSS_SECRET_ACCESS_KEY = 'uNanW3iXuRs4Vao9MUdKWW5DYUeyFT'
-    OSS_STORAGE_BUCKET_NAME = ''
+    OSS_STORAGE_BUCKET_NAME = 'kadaphoto'
     OSS_HEADERS = {
         'Cache-Control': 'max-age=31536000',
     }
