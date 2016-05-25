f0945e6c7e188a06141a7bb58e20e91781028c26
Deploy Server <deployserver@deployserver.local>
2016-05-25 18:38:03 +0800

    fix

--- kada/core/auth/api.py
+++ kada/core/auth/api.py
@@ -10,8 +10,11 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.translation import ugettext_lazy as _
 from django.core import serializers
 from core.models import UserProfile
-from core.auth.utils import get_real_username
+from core.auth.utils import get_real_username, send_vcode, vcode_varified
 from django.core.exceptions import ObjectDoesNotExist
+
+
+VER_PURPOSE_LIST = ['register', 'retrieve']
 
 # Return Message for login
 R = {
@@ -33,10 +36,14 @@ R_VER = {
 }
 
 USER_EXISTS = _('That User Already Exists')
+USER_INVALID = _('That User Does Not Exist')
+INVALID_PURPOSE = _('Invalid Purpose')
+INVALID_PARAM = _('Invalid Parameter')
 USER_AUTHENTICATED = _('User Authenticated')
 USRNAME_PWD_INCORRECT = _('Username or Password Incorrect')
 CREDENTIAL_INCORRECT = _('Credential Incorrect')
 USER_DISABLED = _('User Disabled')
+SUCCEED = _('Succeed')
 
 def user_auth(username, password=None, oauth_token=None, login=False):
     user = None
@@ -106,17 +113,21 @@ def login(request):
 def register(request):
     username = request.POST.get("username", "")
     password = request.POST.get("password", "")
+    vcode = request.POST.get("vcode", "")
 
-    if username + password != "":
-        _user, user_created = User.objects.get_or_create(username=username)
-        if not user_created:
-            # User Exists Change Password
-            R_REG['msg'] = unicode(USER_EXISTS)
-        else:
-            # User Does not Exist
+    if username + password + vcode != "":
+        if vcode_varified(username, vcode):
+            _user, user_created = User.objects.get_or_create(username=username)
             _user.set_password(password) 
+            R_REG['msg'] = unicode(SUCCEED)
+            R_REG['register_succeed'] = 1
+            if user_created:
+                profile = UserProfile.objects.get(user=_user)
+                R_REG['user_info'] = json.loads(serializers.serialize('json', [user]))[0]['fields']
+                R_REG['user_profile'] = json.loads(serializers.serialize('json', [profile]))[0]['fields']
     else:
-        pass
+        R_REG['msg'] = unicode(INVALID_PARAM)
+        R_REG['register_succeed'] = 0
         # Wrong
     return JsonResponse(R_REG)
 
@@ -124,12 +135,23 @@ def register(request):
 @csrf_exempt
 def verify(request):
     username = request.POST.get("username", "")
+    purpose = request.POST.get("purpose", "")  
     real_username = get_real_username(username)
-    try:
-        _user = User.objects.get(username=real_username)
-    except ObjectDoesNotExist:
-        print _user
-    # R_VER['msg'] = unicode(USER_EXISTS)
-    # R_VER['v_code_sent'] = 1
-    r = "This is a Dummy API %s " % real_username
-    return HttpResponse(r)
+    if purpose in VER_PURPOSE_LIST:
+        try:
+            _user = User.objects.get(username=real_username)
+            if purpose == 'register':
+                R_VER['msg'] = unicode(USER_EXISTS)
+                R_VER['v_code_sent'] = 0
+            else:
+                R_VER['msg'], R_VER['v_code_sent'] = send_vcode(real_username, username)
+        except ObjectDoesNotExist:
+            if purpose == 'retrieve':
+                R_VER['msg'] = unicode(USER_INVALID)
+                R_VER['v_code_sent'] = 0
+            else:
+                R_VER['msg'], R_VER['v_code_sent'] = send_vcode(real_username, username)
+    else:
+        R_VER['msg'] = unicode(INVALID_PURPOSE)
+        R_VER['v_code_sent'] = 0
+    return JsonResponse(R_VER)
