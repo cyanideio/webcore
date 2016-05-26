ecc55f7dfe12d0ddc383c3eac94f7b06a610154e
Deploy Server <deployserver@deployserver.local>
2016-05-26 16:02:05 +0800

    fix

--- kada/core/api/advertise.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from kada.utils.resource import KadaResource
-from core.models import Advertise
-
-class AdvertiseResource(KadaResource):
-    class Meta:
-        queryset = Advertise.objects.all()
