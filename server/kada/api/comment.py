ceeb8f0b790e8e3af899db107544af102e2bf518
Deploy Server <deployserver@deployserver.local>
2016-05-30 11:36:31 +0800

    fix

---
+++ server/kada/api/comment.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from core.utils.resource import BaseResource
+from core.utils.auth import BaseAuthentication
+from kada.models import Comment
+
+class CommentResource(BaseResource):
+    class Meta:
+        queryset = Comment.objects.all()
+        authentication = BaseAuthentication()
