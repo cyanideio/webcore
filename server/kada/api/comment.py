777378a9ff8886bd42c13ad67fff3f740f9f2e7f
Deploy Server <deployserver@deployserver.local>
2016-06-03 18:54:02 +0800

    fix

--- server/kada/api/comment.py
+++ server/kada/api/comment.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 from core.utils.resource import BaseResource
 from core.utils.auth import BaseAuthentication
 from tastypie.authorization import Authorization
+from core.api.user import UserResource
 from tastypie.fields import ForeignKey
 from kada.models import Comment
 from kada.api.gallery import GalleryResource
 
 class CommentResource(BaseResource):
+    author = fields.ToOneField(UserResource, 'author', null=True, full=True)
     gallery = ForeignKey(GalleryResource, 'gallery')
-
     class Meta:
         queryset = Comment.objects.all()
         authentication = BaseAuthentication()
@@ -18,8 +19,6 @@ class CommentResource(BaseResource):
         filtering = {
             'gallery': ('exact')
         }
-
-
     def hydrate(self, bundle, request=None):
         bundle.obj.author = User.objects.get(pk = bundle.request.user.id)
         return bundle 
