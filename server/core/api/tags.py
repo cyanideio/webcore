e76c0a0fac38b16c2a373a0332d909d75d67ca3f
Frank <knarfytrebil@gmail.com>
2016-06-12 21:06:38 +0800

    fix

--- server/core/api/tags.py
+++ server/core/api/tags.py
@@ -1,9 +1,15 @@
 from taggit.models import Tag
 from core.utils.resource import BaseResource
-from core.utils.auth import BaseAuthentication, ReadOnlyAuthorization
+from core.utils.auth import BaseAuthentication
+from tastypie.authorization import ReadOnlyAuthorization
+from tastypie.resources import ALL_WITH_RELATIONS, ALL
 
 class TagResource(BaseResource):
     class Meta:
         authentication = BaseAuthentication()
         authorization = ReadOnlyAuthorization()
         queryset = Tag.objects.all()
+
+        filtering = {
+            'name': ('exact'),
+        }
