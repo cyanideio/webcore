f60f5b15b85b9d745b1a4a70d6da2dd76fbdad49
Deploy Server <deployserver@deployserver.local>
2016-06-14 17:50:15 +0800

    fix

--- server/kada/api/gallery.py
+++ server/kada/api/gallery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import base64
 from django.contrib.auth.models import User
 from django.db.models import Count
 from tastypie.fields import ListField
@@ -122,7 +123,7 @@ class GalleryShareResource(GalleryResource):
 
     def obj_get(self, bundle, **kwargs):
         try:
-            pk = kwargs['pk'][4:-4]
+            pk = base64.b64decode(kwargs['pk'][4:-4])
         except Exception:
             raise self._meta.object_class.DoesNotExist("Couldn't find an instance")
         kwargs['pk'] = pk
