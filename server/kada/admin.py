22be9993cde3ca28f9018ca279720cf4572167fe
root <frank.yao@cyanide.io>
2016-06-15 19:29:31 +0800

    fix

--- server/kada/admin.py
+++ server/kada/admin.py
@@ -11,7 +11,7 @@ from super_inlines.admin import SuperInlineModelAdmin, SuperModelAdmin
 from kada.models import Gallery, Comment, Photo, SceneTemplate, Scene, ServiceType, Service, Advertise, Tip, Feedback, SceneSet, PhotoFrame, GalleryTags
 
 admin.site.register(Gallery, CommonAdmin)
-admin.site.register(GalleryTag, CommonAdmin)
+admin.site.register(GalleryTags, CommonAdmin)
 admin.site.register(Comment, CommonAdmin)
 admin.site.register(Photo, CommonAdmin)
 admin.site.register(SceneTemplate, CommonAdmin)
