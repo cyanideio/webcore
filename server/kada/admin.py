26b32f781e73913b2e84c997336988587f505bb0
Deploy Server <deployserver@deployserver.local>
2016-06-15 19:26:34 +0800

    fix

--- server/kada/admin.py
+++ server/kada/admin.py
@@ -8,9 +8,10 @@ from super_inlines.admin import SuperInlineModelAdmin, SuperModelAdmin
 
 
 # Register your models here.
-from kada.models import Gallery, Comment, Photo, SceneTemplate, Scene, ServiceType, Service, Advertise, Tip, Feedback, SceneSet, PhotoFrame
+from kada.models import Gallery, Comment, Photo, SceneTemplate, Scene, ServiceType, Service, Advertise, Tip, Feedback, SceneSet, PhotoFrame, GalleryTag
 
 admin.site.register(Gallery, CommonAdmin)
+admin.site.register(GalleryTag, CommonAdmin)
 admin.site.register(Comment, CommonAdmin)
 admin.site.register(Photo, CommonAdmin)
 admin.site.register(SceneTemplate, CommonAdmin)
