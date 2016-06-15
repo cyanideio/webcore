f9af3ff2f2e508e5a6de81d94efb69a6b0de0703
Deploy Server <deployserver@deployserver.local>
2016-06-15 12:03:41 +0800

    fix

--- server/core/admin.py
+++ server/core/admin.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.auth.models import User
+
 # Register your models here.
 from core.models import Friend, Message, UserProfile
+from django.contrib.auth.admin import UserAdmin
 
 class CommonAdmin(admin.ModelAdmin):
     list_display = ('id', 'created', 'updated')
 
+admin.site.unregister(User)
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [ UserProfileInline, ]
+
+admin.site.register(User, UserProfileAdmin)
+
 admin.site.register(Friend, CommonAdmin)
 admin.site.register(Message, CommonAdmin)
-admin.site.register(UserProfile, CommonAdmin)
+# admin.site.register(UserProfile, CommonAdmin)
