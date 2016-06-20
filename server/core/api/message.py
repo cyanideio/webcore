e8aca4b1340cf65d2319a2a768bf68e6769b4723
Deploy Server <deployserver@deployserver.local>
2016-06-20 15:04:44 +0800

    fix

--- server/core/api/message.py
+++ server/core/api/message.py
@@ -5,6 +5,7 @@ from core.utils.resource import BaseResource
 from core.utils.auth import MessageAuthorization, BaseAuthentication
 from core.api.user import UserResource
 from core.models import Message
+from kada.models import Gallery
 
 class MessageResource(BaseResource):
     author = fields.ToOneField(UserResource, 'author', null=True, full=True)
@@ -16,3 +17,10 @@ class MessageResource(BaseResource):
         queryset = Message.objects.all()
         authentication = BaseAuthentication()
         authorization = MessageAuthorization()
+
+    def dehydrate(self, bundle):
+    	if bundle.data['msg_type'] in [1,0]:
+    		target_id = int(bundle.data['jump_target'])
+    		name = Gallery.objects.get(id=target_id).name
+    		bundle.data['name'] = name
+        return bundle
