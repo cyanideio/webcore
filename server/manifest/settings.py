98e1ab1430d5ae0a9b79d120d5d3102b6bd2169c
Frank <knarfytrebil@gmail.com>
2016-06-28 00:19:54 +0800

    fix

--- server/manifest/settings.py
+++ server/manifest/settings.py
@@ -114,3 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if MANIFEST_ENV == 'server':
+    STATIC_ROOT = '/usr/share/nginx/static'
+    MEDIA_ROOT = '/usr/share/nginx/media'
--- server/manifest/wsgi.py
+++ server/manifest/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kada.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manifest.settings")
 
 application = get_wsgi_application()
