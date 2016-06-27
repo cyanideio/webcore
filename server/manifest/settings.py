146e82ec17af1bf8c86e8965ebc960e6ad9722fa
Frank <knarfytrebil@gmail.com>
2016-06-27 20:27:43 +0800

    fix

--- server/manifest/settings.py
+++ server/manifest/settings.py
@@ -3,6 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MANIFEST_ENV = os.environ['MANIFEST_ENV']
+PASSWORD = os.environ['DJANGO_PASS']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -86,7 +87,7 @@ elif MANIFEST_ENV == 'server':
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'webcore',                      
             'USER': 'webcore',
-            'PASSWORD': '',
+            'PASSWORD': PASSWORD,
             'HOST': 'localhost',
             'PORT': '5432',
         }
