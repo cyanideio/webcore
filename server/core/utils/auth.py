952168067f80c6a0a75da0ccf808b1645086c25e
Deploy Server <deployserver@deployserver.local>
2016-07-07 12:54:48 +0800

    fix

--- server/core/utils/auth.py
+++ server/core/utils/auth.py
@@ -221,3 +221,29 @@ class WriteOnlyAuthorization(Authorization):
 
     def delete_detail(self, object_list, bundle):
         raise Unauthorized("Sorry, no deletes.") 
+
+class UserAuthorization(Authorization):
+
+    def read_list(self, object_list, bundle):
+        raise Unauthorized("Sorry, no details")
+
+    def read_detail(self, object_list, bundle):
+        return True
+
+    def create_list(self, object_list, bundle):
+        return object_list
+
+    def create_detail(self, object_list, bundle):
+        raise Unauthorized("Sorry, no updates.")
+
+    def update_list(self, object_list, bundle):
+        raise Unauthorized("Sorry, no updates.")
+
+    def update_detail(self, object_list, bundle):
+        raise Unauthorized("Sorry, no updates.")
+
+    def delete_list(self, object_list, bundle):
+        raise Unauthorized("Sorry, no deletes.")
+
+    def delete_detail(self, object_list, bundle):
+        raise Unauthorized("Sorry, no deletes.") 
